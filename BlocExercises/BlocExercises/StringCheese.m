9fce54708003f9851d1c4f0abb80e4d7ea857da9
Peter Scheyer <pscheyer@gmail.com>
2015-01-12 13:55:03 +0300

    Mostly Completed Strings Assignment 20150112 1354Qtime

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -11,20 +11,26 @@
 @implementation StringCheese
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+   NSString *fullSentence = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    return fullSentence;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSRange cheeseInName = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+    NSString *cheeseNameWithoutCheeseSuffix = [cheeseName stringByReplacingCharactersInRange:cheeseInName withString:@""];
+    return cheeseNameWithoutCheeseSuffix;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        NSString *modifiableStatement = [NSString stringWithFormat:@"%ld cheese", (long)cheeseCount];
+        return modifiableStatement;
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        NSString *modifiableStatement = [NSString stringWithFormat:@"%ld cheeses", (long)cheeseCount];
+        return modifiableStatement;
     }
     
     /*
