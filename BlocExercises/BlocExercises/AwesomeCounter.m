4306fbbd415db497475840da117d0f55eea369ed
Peter Scheyer <pscheyer@gmail.com>
2015-01-16 13:13:03 +0300

    Issues with Loops  20150116 1312Qtime

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,49 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
-}
+//    if both are same
+    NSString *numberForOutput = [NSString stringWithFormat:@"%ld", (long)number];
+    NSMutableString *outputString = [NSMutableString stringWithString:numberForOutput];
+    if (number == otherNumber) {
+        //        return first.
+        return outputString;
+    } else if (number > otherNumber) {
+   //        If first > second
+        NSInteger *larger = &number;
+        NSInteger *smaller = &otherNumber;
+        NSInteger *largerPlus = larger + 1;
+//        assign first to ‘larger’
+//        assign second to ‘smaller’
+//        while loop
+        while (larger > smaller) {
+            NSString *smallerForOutput = [NSString stringWithFormat:@"%ld", (long)smaller];
+        [outputString appendString:smallerForOutput];
+            smaller = smaller + 1;
+        }
+        return outputString;
+    } else {
+        //        else
+        NSInteger *smaller = &number;
+        NSInteger *larger = &otherNumber;
+//            assign second to ‘larger’
+//            assign first to ‘smaller’
+//        While loop
+        while (larger > smaller) {
+            NSString *smallerForOutput = [NSString stringWithFormat:@"%ld", (long)smaller];
+            [outputString appendString:smallerForOutput];
+            smaller = smaller + 1;
+        }
+        return outputString;
+    }
+    
 
+
+//            
+//            Loop:
+//            While smaller != larger
+//            append ‘smaller’ to ‘output string’
+//            add 1 to ‘smaller’
+//            
+//            return ‘output string.’
+}
 @end
