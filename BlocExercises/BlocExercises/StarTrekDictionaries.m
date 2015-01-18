b1a7b03206289ca32327d137f8b62b741e69adac
Peter Scheyer <pscheyer@gmail.com>
2015-01-18 11:03:46 +0300

    Completed Dictionaries Assignment 20150118 1103Qtime

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -13,19 +13,28 @@
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
 //    NSMutableDictionary *favDrinkDictMutable= [characterDictionary mutableCopy];
-//    NSString *favDrinkString = [characterDictionary[@"favorite drink"] string];
-//    NSString *stringForOutput = [NSString stringWithFormat:@"%@", favDrinkString];
-    return [characterDictionary[@"favorite drink"] string];
+    NSString *favDrinkString = characterDictionary[@"favorite drink"];
+    NSString *stringForOutput = [NSString stringWithFormat:@"%@", favDrinkString];
+    return stringForOutput;
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    NSMutableArray  *mutArrayFavDrinksTrek = [[NSMutableArray alloc] init];
+    for (NSDictionary *character in charactersArray) {
+        NSString *charDrink = character[@"favorite drink"];
+        [mutArrayFavDrinksTrek addObject:charDrink];
+    }
+    NSArray *arrayOfFavoriteDrinksForStarTrekCharacters = [mutArrayFavDrinksTrek copy];
+    return arrayOfFavoriteDrinksForStarTrekCharacters;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    NSMutableDictionary *mutDictionaryWithQuoteAddedToStarTrekCharacterDictionary = [characterDictionary mutableCopy];
+    [mutDictionaryWithQuoteAddedToStarTrekCharacterDictionary setValue:@"StarTrekQuote" forKey:(@"quote")];
+     NSDictionary *outputDictionary = mutDictionaryWithQuoteAddedToStarTrekCharacterDictionary;
+    return outputDictionary;
 }
 
 @end
