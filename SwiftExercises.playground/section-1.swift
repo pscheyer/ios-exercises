c5b769c0cae7cd1b4371d77c5c5a311a3efedfd9
Peter Scheyer <pscheyer@gmail.com>
2015-01-25 00:35:07 +0300

    Completed Swift assignment 20150125 0035Qtime

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,6 +8,7 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
+    let cheese = "My favorite cheese is \(cheese)"
     return cheese
 }
 
@@ -20,13 +21,16 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
+numberArray.append(5)
 // WORK HERE
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+numberDictionary[5] = "five"
+
 
 /*
 
@@ -34,8 +38,15 @@ Loops
 
 */
 
+for i in 1...10 {
+    println(i)
+}
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
+
+for i in 1..<11 {
+    println(i)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
@@ -56,6 +67,13 @@ let picard = [
 let characters = [worf, picard]
 
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
+    var favoriteDrinksArrayForCharacters = []
+    for (key, item) in characters {
+        if key == "favorite drink" {
+            favoriteDrinksArrayForCharacters.append(item)
+        }
+        
+    }
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
     return []
@@ -71,13 +89,23 @@ Functions
 
 */
 
+
 // Make a function that inputs an array of strings and outputs the strings separated by a semicolon
 
 let strings = ["milk", "eggs", "bread", "challah"]
 
+func stringToStringsSeparatedBySemicolon(inputArray:Array<String>) -> String {
+    var outputString = ""
+    for (inputString) in inputArray {
+        outputString += "; \(inputString)"
+    }
+    println(outputString)
+}
+
 // WORK HERE - make your function and pass `strings` in
 
-let expectedOutput = "milk;eggs;bread;challah"
+let expectedOutput = stringToStringsSeparatedBySemicolon(strings)
+//"milk;eggs;bread;challah"
 
 /*
 
@@ -89,3 +117,5 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+let cerealArraySorted = sorted(cerealArray, >)
+
